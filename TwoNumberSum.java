4d306549f21a38a372ce8e144e1c55550f8c52a2
Sisihlia Yuniyarti <sisihlia@users.noreply.github.com>
2021-08-19 09:18:28 +0200

    add solution with O(n^2) time | O(1) space

--- TwoNumberSum.java
+++ TwoNumberSum.java
@@ -1,1 +1,24 @@
+import java.util.*;
+
+class Program {
+  public static int[] twoNumberSum(int[] array, int targetSum) {
+    // O(n^2) time | O(1) space.
+		int [] arr = new int [2];
+		if (array.length<2) return new int [0];
+		outerloop:
+		for (int i=0; i<array.length; i++) {
+			for (int j=i+1; j<array.length;j++) {
+				int sum=array[i] + array[j]; 		
+				if(i==array.length-2 && sum!=targetSum ){return new int [0];} 
+				if(sum == targetSum) {
+					arr[0] = array[i];
+					arr[1] = array[j];
+					break outerloop;
+				}
+			}
+		}
+		//System.out.println("Hello");
+    return arr;
+  }
+}
 
